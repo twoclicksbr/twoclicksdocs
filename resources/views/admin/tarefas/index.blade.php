34c9b818fe2c63296c75be8d24f10df73d54d586
Alex Bethel <github@twoclicks.com.br>
2026-05-18 22:22:16 -0300

    fix(admin/tarefas): table-responsive no grid de tarefas

--- resources/views/admin/tarefas/index.blade.php
+++ resources/views/admin/tarefas/index.blade.php
@@ -24,6 +24,7 @@
         </div>
     </div>
     <div class="card-body py-4">
+        <div class="table-responsive">
         <table class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4">
             <thead>
                 <tr class="fw-bold text-muted">
@@ -66,6 +67,7 @@
                 @endforelse
             </tbody>
         </table>
+        </div>
     </div>
 </div>
 
